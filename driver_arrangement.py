6d266870591b24d260d798316f53150c85187c24
Alden <aldenliang0110@gmail.com>
2023-03-08 15:47:22 -0500

    🔧 chage autopep8 setting
    Igored auto line change setting since my monitor is wide enough to show the code.

--- driver_arrangement.py
+++ driver_arrangement.py
@@ -25,18 +25,14 @@ def read_destinations(path) -> list:
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument(
-        "--drivers", help="the path of drivers data file", metavar="<drivers data file path>", required=True)
-    argument_parser.add_argument(
-        "--destinations", help="the path of destinations data file", metavar="<destinations data file path>", required=True)
+    argument_parser.add_argument("--drivers", help="the path of drivers data file", metavar="<drivers data file path>", required=True)
+    argument_parser.add_argument("--destinations", help="the path of destinations data file", metavar="<destinations data file path>", required=True)
     arguments = argument_parser.parse_args()
 
     attached_algorithm = SecretAlgorithm()
     attached_algorithm.set_drivers(read_drivers(arguments.drivers))
-    attached_algorithm.set_destinations(
-        read_destinations(arguments.destinations))
+    attached_algorithm.set_destinations(read_destinations(arguments.destinations))
 
     total_score, matching = arrangement()
     print(f"The optimized total score is {total_score}")
-    print(
-        f"The outcome of the match is a list of tuples presented in the format of (<driver's name>, <destination>)\n{matching}")
+    print(f"The outcome of the match is a list of tuples presented in the format of (<driver's name>, <destination>)\n{matching}")
